45cc4a1288634cfda0db95a7bdc61fceb8f7fa2c
demby_abella <dmcchy@gmail.com>
2021-05-10 01:36:37 +0800

    Moved param validation to service

--- src/v2/api/services/delivery_service.go
+++ src/v2/api/services/delivery_service.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"errors"
 	"github.com/dembygenesis/droppy-prulife/src/v2/api/domain/delivery"
 	"github.com/dembygenesis/droppy-prulife/src/v2/api/repositories"
 	"github.com/dembygenesis/droppy-prulife/src/v2/api/utils"
+	"net/http"
+	"reflect"
 )
 
 type Service interface {
@@ -25,6 +28,52 @@ func NewDeliveryService(deliveryRepo repositories.DeliveryRepository) Service {
 	}
 }
 
+func (s *service) ValidateNoEmptyParams(p *delivery.CreateDelivery) *utils.ApplicationError {
+	var missingParameters []string
+
+	pp := *p
+	v := reflect.ValueOf(pp)
+	v2 := reflect.TypeOf(pp)
+
+	typeOfS := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		propertyType := typeOfS.Field(i).Type.String()
+		propertyValue := v.Field(i).Interface()
+		propertyName := v2.Field(i).Tag.Get("json")
+		required := v2.Field(i).Tag.Get("required")
+
+		if required != "false" {
+			if propertyType == "string" {
+				if propertyValue == "" {
+					missingParameters = append(missingParameters, ``+propertyName+` empty`)
+				}
+			} else if propertyType == "float64" {
+				if propertyValue == 0 {
+					missingParameters = append(missingParameters, ``+propertyName+` empty`)
+				}
+			}
+		}
+	}
+	if len(missingParameters) > 0 {
+		return &utils.ApplicationError{
+			HttpStatus: http.StatusUnprocessableEntity,
+			Message:    "validation errors",
+			Error:      errors.New("missing parameters"),
+			Data:       missingParameters,
+		}
+	} else {
+		return nil
+	}
+}
+
 func (s *service) Create(p *delivery.CreateDelivery) *utils.ApplicationError {
+	// Ensure no empty parameters
+	appError := s.ValidateNoEmptyParams(p)
+
+	if appError != nil {
+		return appError
+	}
+
 	return s.deliveryRepo.Create(p)
 }
