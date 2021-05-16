6c2c232884581d5ef36bde5f0727deca2db410fa
demby_abella <dmcchy@gmail.com>
2021-05-16 17:36:54 +0800

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 # Build go
-env GOOS=linux GOARCH=amd64 go build -o droppy_prulife_go *.go
+env GOOS=linux GOARCH=amd64 go build -o droppy_cgnorthpeak_go *.go
 
 # Stop service
-ssh droppy "service go-droppy-prulife stop"
+ssh droppy "service go-droppy-cgnorthpeak stop"
 
 # Remove File
-ssh droppy "rm /var/www/droppy_prulife_go/droppy_prulife_go"
+ssh droppy "rm /var/www/droppy_cgnorthpeak_go/droppy_cgnorthpeak_go"
 
 # Upload file
-scp droppy_prulife_go droppy:/var/www/droppy_prulife_go
+scp droppy_cgnorthpeak_go droppy:/var/www/droppy_cgnorthpeak_go
 
 # Chmod
-ssh droppy "chmod +x /var/www/droppy_prulife_go/droppy_prulife_go"
+ssh droppy "chmod +x /var/www/droppy_cgnorthpeak_go/droppy_cgnorthpeak_go"
 
 # Stop service
-ssh droppy "service go-droppy-prulife stop"
+ssh droppy "service go-droppy-cgnorthpeak stop"
 
 # Refresh daemons
 ssh droppy "systemctl daemon-reload"
 
 # Start service again
-ssh droppy "service go-droppy-prulife start"
+ssh droppy "service go-droppy-cgnorthpeak start"
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/valyala/fasthttp v1.19.0 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/sys v0.0.0-20210104204734-6f8348627aad // indirect
-	gorm.io/driver/mysql v1.0.6 // indirect
+	gorm.io/driver/mysql v1.0.6
 	gorm.io/driver/sqlite v1.1.4 // indirect
-	gorm.io/gorm v1.21.9 // indirect
+	gorm.io/gorm v1.21.9
 )
